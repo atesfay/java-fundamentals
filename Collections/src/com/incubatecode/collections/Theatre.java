edd2d26e0bdaae9b6f9889d5af7ab026e0f52f8c
atesfay <abrahamkt@yahoo.com>
2019-01-09 21:48:46 -0800

    MapChallange folder pushed

--- Collections/src/com/incubatecode/collections/Theatre.java
+++ Collections/src/com/incubatecode/collections/Theatre.java
@@ -32,20 +32,49 @@ public class Theatre {
 	}	
 	
 	public boolean reserveSeat(String seatNumber){
-		Seat requestedSeat = null;
-		for (Seat seat: seats){
-			if(seat.getSeatNumber().equals(seatNumber)){
-				requestedSeat = seat;
-				break;
-			}
-		}
 		
-		if (requestedSeat == null){
-			System.out.println("There is no seat " + seatNumber);
-			return false;
-		}
 		
-		return requestedSeat.reserve();
+		// = = = = = = = = = =  = = =  = = = = =  = = = = =  = = = =
+		// use binary search compare seats
+		
+		int low = 0;
+        int high = seats.size()-1;
+
+        while (low <= high) {
+            System.out.print(".");
+            int mid = (low + high) / 2;
+            Seat midVal = seats.get(mid);
+            int cmp = midVal.getSeatNumber().compareTo(seatNumber);
+
+            if (cmp < 0) {
+                low = mid + 1;
+            } else if (cmp > 0) {
+                high = mid - 1;
+            } else {
+                return seats.get(mid).reserve();
+            }
+        }
+        System.out.println("There is no seat " + seatNumber);
+        return false;
+        
+		// = = = = = = = = = =  = = =  = = = = =  = = = = =  = = = =
+		
+//		Seat requestedSeat = null;
+//		for (Seat seat: seats){
+//			if(seat.getSeatNumber().equals(seatNumber)){
+//				requestedSeat = seat;
+//				break;
+//			}
+//		}
+//		
+//		if (requestedSeat == null){
+//			System.out.println("There is no seat " + seatNumber);
+//			return false;
+//		}
+//		
+//		return requestedSeat.reserve();
+        
+		// = = = = = = = = = =  = = =  = = = = =  = = = = =  = = = =
 	}
 	
 	// for testing purpose
