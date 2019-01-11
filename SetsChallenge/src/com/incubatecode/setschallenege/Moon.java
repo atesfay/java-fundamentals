d26e05b56339c46dc0797bb52247707995b3bf3c
atesfay <abrahamkt@yahoo.com>
2019-01-11 15:58:33 -0800

    SetsChallenge Folder pushed

---
+++ SetsChallenge/src/com/incubatecode/setschallenege/Moon.java
@@ -0,0 +1,9 @@
+package com.incubatecode.setschallenege;
+
+public class Moon extends HeavenlyBody{
+	
+    public Moon(String name, double orbitalPeriod) {
+        super(name, orbitalPeriod, BodyTypes.MOON);
+    }
+
+}
